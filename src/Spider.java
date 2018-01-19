1395a44aa203f4a80ed6441db36800da9a53edef
tomibeli <tomibeli@10.31.5.97>
2018-01-19 14:25:02 +0000

    Added Spider Class

---
+++ src/Spider.java
@@ -0,0 +1,20 @@
+
+public class Spider extends Animal{
+
+	public Spider(String name) {
+		super(name);
+	}
+	
+	public String getSound() {
+		return "...";
+	}
+	
+	public boolean hasWings() {
+		return false;
+	}
+	
+	public int getNumLegs() {
+		return 8;
+	}
+
+}
